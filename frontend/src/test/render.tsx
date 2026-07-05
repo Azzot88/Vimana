6308d97912eaf0cb35a075250c4ad36143392896
Nick <azzot88@gmail.com>
2026-07-04 20:10:11 -0400

    silence router future warnings

--- frontend/src/test/render.tsx
+++ frontend/src/test/render.tsx
@@ -4,10 +4,14 @@ import { MemoryRouter } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next'
 import i18n from '../i18n'
 
+const routerFuture = { v7_startTransition: true, v7_relativeSplatPath: true } as const
+
 export function renderWithProviders(ui: ReactElement, { route = '/' }: { route?: string } = {}) {
   return render(
     <I18nextProvider i18n={i18n}>
-      <MemoryRouter initialEntries={[route]}>{ui}</MemoryRouter>
+      <MemoryRouter initialEntries={[route]} future={routerFuture}>
+        {ui}
+      </MemoryRouter>
     </I18nextProvider>,
   )
 }
